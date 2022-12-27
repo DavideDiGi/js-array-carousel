02a7753d93f57611fecb9211416199352af3c1a0
Davide Di Girolamo <davide.digi2001@gmail.com>
2022-12-27 20:01:42 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,11 +1,60 @@
-const images = ['img/01.jpg','img/02.jpg','img/03.jpg','img/04.jpg','img/05.jpg'];
+const images = ['img/01.webp','img/02.webp','img/03.webp','img/04.webp','img/05.webp'];
 
-const carousel = document.querySelector('.my-carousel');
+// const carousel = document.querySelector('.my-carousel');
+const slides = document.querySelector('.slides');
 
 for (let i = 0; i < images.length; i++) {
-    
-    carousel.innerHTML += `<div class="slide"
-                              <img src="${images[index]}">
+    console.log(images[i]);
+    slides.innerHTML += `<div class="slide">
+                              <img src="${images[i]}">
                            </div>`;
     
 }
+
+const allSlides = document.querySelectorAll('.slide');
+console.log('allSlides', allSlides, typeof allSlides);
+
+allSlides[0].classList.add('current'); 
+
+let currentSlide = 0;
+
+const nextArrow = document.querySelector('.next')
+const previousArrow = document.querySelector('.previous')
+
+nextArrow.addEventListener('click',
+
+    function () {
+        console.log('cliccato su next');
+        
+        allSlides[currentSlide].classList.remove('current');
+        currentSlide ++;
+
+        allSlides[currentSlide].classList.add('current');
+
+        if (currentSlide == allSlides.length - 1) {
+            nextArrow.classList.add('hidden');
+        }
+
+        previousArrow.classList.remove('hidden');
+    }
+
+);
+
+previousArrow.addEventListener('click',
+
+    function () {
+        console.log('cliccato su previous');
+        
+        allSlides[currentSlide].classList.remove('current');
+        currentSlide --;
+
+        allSlides[currentSlide].classList.add('current');
+
+        if (currentSlide == 0) {
+            previousArrow.classList.add('hidden');
+        }
+
+        nextArrow.classList.remove('hidden')
+    }
+
+);
